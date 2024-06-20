557c640e22c2989377bb14d079b4d2858077ab69
John <141792618+michaellucas2008@users.noreply.github.com>
2024-06-20 01:41:33 -0400

    create a function that ask the user for an input and use the input as the player's choice. It checks if the input is valid or not and increments a counter for every invalid input. Once the counter reaches 10 and above, it force the player's choice to be randomized.

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -17,3 +17,27 @@ function getComputerChoice(){
     console.log("Computer chooses:"+computerChoice);
     return computerChoice;
 }
+
+function getHumanChoice(){
+    let userInput=prompt("Enter your choice (Rock, Paper, Scissor)");
+    let isValid=true;
+    let counter=0;
+    while(isValid){
+        if(!("ROCK"===userInput.toUpperCase()
+            ||"PAPER"===userInput.toUpperCase()
+            ||"SCISSOR"===userInput.toUpperCase())){
+                userInput=prompt("Invalid Choice. Please try again");
+                if(counter>10){
+                    alert("Player's choice will be randomized");
+                    userInput=getComputerChoice();
+                    isValid=false;
+                }
+            }
+        else{
+            isValid=false;
+        }
+        counter++;
+    }
+    console.log("Player chooses:"+userInput);
+    return userInput;
+}
