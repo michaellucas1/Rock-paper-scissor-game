21d1c4411b2d2f269f5cd1192aea51f239848b14
John <141792618+michaellucas2008@users.noreply.github.com>
2024-06-20 01:36:39 -0400

    add a function that uses a math function to randomly make a choice for the computer

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -1,3 +1,19 @@
 let humanScore=0;
 let computerScore=0;
 let gameCount=0;
+
+function getComputerChoice(){
+    const randomNumber=Math.floor(Math.random()*3)+1;
+    let computerChoice="";
+    if(randomNumber===1){
+        computerChoice+="Rock";
+    }
+    else if(randomNumber===2){
+        computerChoice+="Paper";
+    }
+    else{
+        computerChoice+="Scissor";
+    }
+    console.log("Computer chooses:"+computerChoice);
+    return computerChoice;
+}
